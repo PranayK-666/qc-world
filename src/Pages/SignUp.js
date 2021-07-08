f5fe7878d7c90c8690c6d32b78e222698bb65404
Pranay Khosla <pranayk@Pranays-MacBook-Air.local>
2021-07-08 15:33:03 +0530

    extras

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,5 +1,9 @@
 import '../App.css';
 
 export default function SignUp() {
-  return <h1 className="sign-up">SIGN UP</h1>;
+  return (
+    <h1 className="sign-up" style={{ color: 'black' }}>
+      Comming Soon....
+    </h1>
+  );
 }
