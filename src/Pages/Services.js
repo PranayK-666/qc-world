d576d1ebc3ba0ce443e503bb2b476694f0c5a734
Pranay Khosla <pranayk@Pranays-MacBook-Air.local>
2021-07-12 15:07:11 +0530

    more blogs added

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
-import '../Pages/services.css'
-import Blogcard from './Blogcard'
+import React from 'react';
+// import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import '../Pages/services.css';
+import Blogcard from './Blogcard';
 
 export default function Services() {
   return (
-    <div className="container fluid-md inline" >
+    <div className="container fluid-md inline">
       <Blogcard />
     </div>
-  )
+  );
 }
